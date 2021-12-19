126047d8e6d54062038ae75d6ccf7fa9ea8b6482
Caleb <calebncontreras5@gmail.com>
2021-12-18 19:29:35 -0800

    - line 36 - extracted color object from project image - line 37 - processed color object and returned a list of rgb values into 'pallet'
    - line 17 - 24: created draw row function
    
    - line 47 - 52: defined outer loop setting the number of rows to 10 and the row gap to 50px. Reset turtle position after each row.

--- main.py
+++ main.py
@@ -14,21 +14,42 @@ def extract_colors(obj):
     return color_pallet
 
 
+def draw_row(row_length):
+    """draws a row of row_length and randomly colored dots each dot 50 pixels apart"""
+    for _ in range(row_length):
+        t.pencolor(random_color())
+        t.pendown()
+        t.forward(1)
+        t.penup()
+        t.forward(50)
+
+
 def random_color():
-    '''returns a random color from a list of colors'''
+    """returns a random color from a list of colors"""
     return random.choice(pallet)
+
 
 tim = t
 screen = t.Screen()
-
-# configure turtle color graphics
-tim.colormode(255) # set color mode to rgb values
-tim.pensize(20) # pen size to 20px
-tim.setposition(-100, -100) # set start position
-x_coordinate = -100
-y_coordinate = -100
 
 # def draw_dots():
 # extracts 20 colors from an image
 color_obj = cg.extract("./Addictive---detail-of-Dam-007.jpg", 20)
 pallet = extract_colors(color_obj)
+
+# configure turtle color graphics
+tim.colormode(255)  # set color mode to rgb values
+tim.pensize(20)  # pen size to 20px
+tim.setposition(-100, -100)  # set start position
+x_coordinate = -100
+y_coordinate = -100
+
+# stop drawing as turtle moves back to beginning of row
+tim.penup()
+for _ in range(10):
+    draw_row(10)  # set number of dots in each row
+    y_coordinate += 50  # move row up by 50px after finished with a row
+    tim.setposition(-100, y_coordinate)  # reset row to new left of screen
+
+print(pallet)
+screen.exitonclick()
